28d14744677789c06a832af49f8d29738f9fd50e
Cristian Bernardo <cristian.silva@ceca.ufal.br>
2021-01-08 20:10:29 -0300

    Finish front-web

---
+++ front-web/src/Orders/OrderSummary.tsx
@@ -0,0 +1,27 @@
+import { formatPrice } from "./helpers";
+
+type Props = {
+    amount: number;
+    totalPrice: number;
+    onSubmit: () => void;
+}
+
+function OrderSummary({ amount, totalPrice, onSubmit }: Props) {
+    return (
+        <div className="order-summary-container">
+            <div className="order-summary-content">
+                <div>
+                <span className="amount-selected-container">
+                    <strong className="amout-selected">{amount} </strong>
+                    PEDIDOS SELECIONADOS
+                </span>
+                <span className="order-summary-total"></span>
+                <strong className="amout-selected">{formatPrice(totalPrice)} </strong>
+                VALOR TOTAL
+                </div>
+                <button className="order-summary-make-order" onClick={onSubmit}>FAZER PEDIDO</button>
+            </div>
+        </div>
+    );
+}
+export default OrderSummary;
