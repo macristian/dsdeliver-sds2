63eee27edbe2c5f4e5f7896fed0351695d12c69a
Cristian Bernardo <cristian.silva@ceca.ufal.br>
2021-01-09 17:29:11 -0300

    Add Orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,14 +1,24 @@
 import { OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableNativeFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+      navigation.navigate('Home');
+  }
+
   return (
+    <TouchableNativeFeedback onPress={handleOnPress}>
     <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
       <Text style={styles.text}>DS Delivery</Text>
     </View>
+    </TouchableNativeFeedback>
   );
 }
 
